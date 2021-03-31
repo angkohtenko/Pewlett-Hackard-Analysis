cfd162b17e8aa954e0aa998957bcf86dfddb9aa2
Angela <angkohtenko@gmail.com>
2021-03-31 16:31:09 -0400

    Deliverable 2 completed

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -40,7 +40,28 @@ FROM
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
-
-
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title
+INTO mentorship_eligibilty
+FROM
+	employees as e
+	INNER JOIN
+		dept_emp as de
+		ON de.emp_no = e.emp_no
+	INNER JOIN
+		titles as tt
+		ON tt.emp_no = e.emp_no
+WHERE 
+	(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND
+	(de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
 
 
